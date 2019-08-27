cfed2219f4431d9455e87a045336868105265e94
Tim Darrow <timothy.honor7@gmail.com>
2019-08-27 07:52:02 -0700

    Added Angular files

---
+++ src/app/media-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mw-media-item',
+    templateUrl: 'media-item.component.html',
+    styleUrls: ['media-item.component.css']
+})
+export class MediaItemComponent {
+    name = 'The Redemption';
+    wasWatched() {
+        return true;
+    };
+ }
